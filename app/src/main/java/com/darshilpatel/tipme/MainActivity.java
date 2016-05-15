9fe17001845776f6e48f20d3b089c03a8bd7bc5c
darshilpatel <darshil-patel@hotmail.com>
2016-05-15 14:27:55 -0400

    working on calculations

--- app/src/main/java/com/darshilpatel/tipme/MainActivity.java
+++ app/src/main/java/com/darshilpatel/tipme/MainActivity.java
@@ -2,12 +2,42 @@ package com.darshilpatel.tipme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final NumberFormat currentFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+    double billAmount = 0.0;
+    double percent = 0.15;
+    TextView amountTextView;
+    TextView percentTextView;
+    TextView tipTextView;
+    TextView totalTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amountTextView = (TextView)findViewById(R.id.amountTextView);
+        percentTextView = (TextView)findViewById(R.id.percentTextView);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+
+        tipTextView.setText(currentFormat.format(0));
+        totalTextView.setText(currentFormat.format(0));
+
+        EditText amountEditText = (EditText)findViewById(R.id.amountTextView);
+        amountEditText.addTextChangedListener(amountEditTextWatcher);
+
+        SeekBar percentSeekBar = (SeekBar)findViewById(R.id.percentSeekBar);
+        percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
     }
 }
